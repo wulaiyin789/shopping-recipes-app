66c94a10c24c015617f0553e1c92121e0540d107
Peter Wu <53519949+wulaiyin789@users.noreply.github.com>
2020-01-15 15:15:01 +0800

    Update files

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global State of the app
@@ -20,6 +21,8 @@ const controlSearch = async () => {
 
     // 1. Get the query from view
     const query = searchView.getInput(); //TODO
+    // TESTING
+    // const query = 'pizza';
 
     if(query) {
         
@@ -31,12 +34,17 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchResult);
         
-        // 4. Search for recipes
-        await state.search.getResults();
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
 
-        // 5. Render the results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5. Render the results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch(error) {
+            alert(`Something went wrong with the search :'(`);
+            clearLoader();
+        }
 
     }
 
@@ -46,6 +54,12 @@ elements.searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     controlSearch();
 });
+
+// TESTING
+// window.addEventListener('load', (e) => {
+//     e.preventDefault();
+//     controlSearch();
+// });
 
 elements.searchResultPages.addEventListener('click', (e) => {
     const btn = e.target.closest('.btn-inline');
@@ -60,4 +74,48 @@ elements.searchResultPages.addEventListener('click', (e) => {
 /**
  * RECIPE CONTROLLER
  */
-const r = new Recipe();
+const controlRecipe = async () => {
+    // Get ID from the URL
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if(id) {
+
+        // 1. Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // EXTRA: Highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
+
+        // 2. Create new recipe object
+        state.recipe = new Recipe(id);
+        // TESTING
+        // window.r = state.recipe;
+
+        try {
+            // 3. Get the recipe data and parse ingredients
+            await state.recipe.getRecipe();
+            // console.log(state.recipe.ingredients);
+            state.recipe.parsingIngredients();
+            // console.log(state.recipe);
+
+            // 4. Calculate servings and time
+            state.recipe.calculateTime();
+            state.recipe.calculateServings();
+
+            // 5. Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
+        } catch(error) {
+            alert('Error processing recipe!');
+            console.log(error);
+        }
+
+    }
+};
+
+//  window.addEventListener('hashchange', controlRecipe);
+//  window.addEventListener('load', controlRecipe);
+['hashchange', 'load'].forEach((e) => window.addEventListener(e, controlRecipe));
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -1,18 +1,99 @@
 import axios from 'axios';
 
 class Recipe {
-    constructor(query) {
-        this.query = query;
-        // this.id = id;
+    constructor(id) {
+        this.id = id;
     }
 
     async getRecipe() {
         try {
             // const res = await axios(`https://forkify-api.herokuapp.com/api/get?key=${key}&rId=${this.id}`);
-            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
+            this.title = res.data.recipe.title;
+            this.author = res.data.recipe.publisher;
+            this.img = res.data.recipe.image_url;
+            this.url = res.data.recipe.source_url;
+            this.ingredients = res.data.recipe.ingredients;
         } catch(error) {
             console.log(error);
+            alert(`Something went wrong :'(`);
         }
+    }
+
+    calculateTime() {
+        // Assuming that we need 15 mins for each 3 ingredients
+        const numImg = this.ingredients.length;
+        const periods = Math.ceil(numImg / 3);
+        this.time = periods * 15;
+    }
+
+    calculateServings() {
+        this.servings = 4;
+    }
+
+    parsingIngredients() {
+
+        const unitsLong = ['tablespoons', 'tablespoon', 'ounces', 'ounce', 'teaspoons', 'teaspoon', 'cups', 'pounds'];
+        const unitsShort = ['tbsp', 'tbsp', 'oz', 'oz', 'tsp', 'tsp', 'cup', 'pound'];
+        const units = [...unitsShort, 'kg', 'g'];
+
+        const newIngredients = this.ingredients.map(el => {
+            
+            // 1. Uniform units
+            let ingredient = el.toLowerCase();
+            unitsLong.forEach((unit, i) => {
+                ingredient = ingredient.replace(unit, unitsShort[i]);
+            });
+
+            // 2. Remove parentheses
+            ingredient = ingredient.replace(/ *\([^)]*\) */g, ' ');
+
+            // 3. Parse ingredients into count, unit and ingredient
+            const arrIng = ingredient.split(' ');
+            const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
+
+            let objIngredient;
+            if(unitIndex > -1) {
+                // There is a unit
+                // E.g. 4 1/2 cups, arrCount is [4, 1/2]  ---> "4+1/2" ---> 4.5
+                // E.g. 4 cups, arrCount is [4]
+                const arrCount = arrIng.slice(0, unitIndex);
+
+                let count;
+                if(arrCount.length === 1) {
+                    count = eval(arrIng[0].replace('-', '+'));
+                } else {
+                    count = eval(arrIng.slice(0, unitIndex).join('+'));
+                }
+
+                objIngredient = {
+                    count,
+                    unit: arrIng[unitIndex],
+                    ingredient: arrIng.slice(unitIndex + 1).join(' ')
+                }
+
+            } else if(parseInt(arrIng[0], 10)) {
+                // There is no unit, but 1st element is number
+                objIngredient = {
+                    count: parseInt(arrIng[0], 10),
+                    unit: '',
+                    ingredient: arrIng.slice(1).join(' ')
+                };
+
+            } else if(unitIndex === -1) {
+                // There is no unit and NO number in 1st number
+                objIngredient = {
+                    count: 1,
+                    unit: '',
+                    ingredient
+                };
+            }
+            
+            return objIngredient;
+
+        });
+
+        this.ingredients = newIngredients;
     }
 }
 
